56eb57bc51965edd6d1c78fffe2e127331560e46
Leo Lomardo <leoland771@gmail.com>
2022-09-12 15:56:07 -0300

    att

--- converteutf.c
+++ converteutf.c
@@ -3,58 +3,42 @@
 
 #include "converteutf.h"
 
-/*
-int confereTipo(arquivo_entrada){
-    unsigned int byteConfere;
-    fread(byteConfere,sizeof(int),1, arquivo_entrada);
 
-    if((byteConfere&0xFEFF) == 0xFEFF || (byteConfere&0xFEFF) == 0xFFFE0000){
-        converteUtf32Para8(arquivo_entrada, arquivo_saida);
-    }else
-        converteUtf32Para8(arquivo_entrada, arquivo_saida);
-
-
-}*/
-
-
-    //ele pega o 1 byte = 1 char, verifica se o bit mais significativo é 0 11 111 1110, dividindo em 4 casos possíveil
-int converteUtf8Para32(FILE *arquivo_entrada, FILE *arquivo_saida){
-    unsigned int auxLeitura;   //caracter que le byte por byte 
-    char byteSaida[4] = {0x00, 0x00, 0x00, 0x00};
-
-    for(auxLeitura = fgetc(arquivo_entrada); auxLeitura !=EOF; auxLeitura = fgetc(arquivo_entrada)){
-        //realizei a leitura utilizando unsigned int, ja que o caracter de maior tamanho e 4 bytes
-            //8 bists char 
-        if(auxLeitura <= 0x7F){
-            byteSaida[0] = auxLeitura;
-            //16 bits char
-            fwrite(byteSaida, sizeof(char), 1, arquivo_saida);
-        }else if(auxLeitura <= 0x7FF){
-            byteSaida[0] = 0xC0 | (auxLeitura >> 6); //110xxxxx || auxLeitura 6 bits pra direita para armazenar os dados do 1 byte
-            byteSaida[1] = 0x80 | (auxLeitura & 0x3F); //10xxxxxx || auxLeitura & 0x3F
-            fwrite(byteSaida, sizeof(char), 2, arquivo_saida);
-            //24 bits char
-        }else if(auxLeitura <=0xFFFF){
-            byteSaida[0] = 0xE0 | (auxLeitura >> 12); //1110xxxx || 1 byte auxLeitura 3 bits pra direita
-            byteSaida[1] = 0x80 | ((auxLeitura >> 6) & 0x3F); //10xxxxxx || auxLeitura 2 bits para direita 
-            byteSaida[2] = 0x80 | (auxLeitura & 0X3F); //10xxxxxx || auxLeitura 2 bits pra direita
-            fwrite(byteSaida, sizeof(char), 3, arquivo_saida);
-
-            //32 bits
-        }else if(auxLeitura <= 0x10FFFF){
-            byteSaida[0] = 0xF0 | (auxLeitura >> 18); //11110xxx ||  1 byte auxLeitura 3 bits pra direita
-            byteSaida[1] = 0x80 | ((auxLeitura >> 12) & 0X3F); //10xxxxxx || auxLeitura 2 bits para direita 
-            byteSaida[2] = 0x80 | ((auxLeitura >> 6) & 0X3F); //10xxxxxx || auxLeitura 2 bits pra direita
-            byteSaida[2] = 0x80 | (auxLeitura & 0x3F); //10xxxxxx || auxLeitura 2 bits pra direita
-            fwrite(byteSaida, sizeof(char), 4, arquivo_saida);
-
-        }else{
-            printf("Caracter fora do alcance.c\n");
-        }
+//le o byte e ve se precisa do byte seguinte para montar o caractere
+int utf8_size(unsigned char aux){
+    if((aux>>7) == 0x00){
+        printf("Char de 1 byte");
+        return 1;
+    }else if((aux >> 5) == 0xC0){
+        printf("Char de 2 byte");
+        return 2;
+    }else if((aux>>4) == 0xE0){
+        printf("Char de 3 byte");
+        return 3;
+    }else if((aux>>5) == 0xF0){
+        printf("Char de 4 byte");
+        return 4;
+    }else{
+        printf("Fora do alcance");
+        return -1;
     }
-    //como esta escrevendo em utf32 bits, falta implementar o BOM
-    //PRECISA VERIRICAR SE ESTÁ ESCREVENDO NA ORDENACAO LITTLE ENDIAN, talvez precise fazer uma funcao aux void Imprime( char *byteSaida, int numBytesSaida)
+}
+//numero de caracteres no arquivo( == numero de bytes)
+int numChar(FILE *arquivo_entrada){
+    char c;
+    int count = 0;
+    for(c = fgetc(arquivo_entrada); c!=EOF; c = fgetc(arquivo_entrada)){
+        c+=1;
+    }
+    return c--;
+}
+
+int converteUtf8Para32(FILE *arquivo_entrada, FILE *arquivo_saida){
+    char bom [4] = {0xFF, 0xFE, 0x00, 0x00};
+    fwrite(bom, sizeof(bom),1, arquivo_saida);
+
+    
 }
 int converteUtf32Para8(FILE *arquivo_entrada, FILE *arquivo_saida){
-
+    
 }
--- testeconv.c
+++ testeconv.c
@@ -1,66 +1,117 @@
 #include "converteutf.h"
 #include <stdio.h>
 
-int countChar(FILE *arquivo_entrada){ //conta quantos caracteres tem no arquivo
-    int count  = 0;
-    char c;
-    for(c = fgetc(arquivo_entrada); c !=EOF; c = fgetc(arquivo_entrada))
-        count = count + 1;
-    
-    rewind(arquivo_entrada);
-    return count--; //reduz 1 para tirar o /n
+//le o byte e ve se precisa do byte seguinte para montar o caractere
+int utf8_size(unsigned char aux){
+    if((aux>>7) == 0x00){
+        printf("Char de 1 byte");
+        return 1;
+    }else if((aux >> 5) == 0xC0){
+        printf("Char de 2 byte");
+        return 2;
+    }else if((aux>>4) == 0xE0){
+        printf("Char de 3 byte");
+        return 3;
+    }else if((aux>>5) == 0xF0){
+        printf("Char de 4 byte");
+        return 4;
+    }else{
+        printf("Fora do alcance");
+        return -1;
+    }
 }
-
+//numero de caracteres no arquivo( == numero de bytes)
+int numChar(FILE *arquivo_entrada){
+    char c;
+    int count = 0;
+    for(c = fgetc(arquivo_entrada); c!=EOF; c = fgetc(arquivo_entrada)){
+        c+=1;
+    }
+    return c--;
+}
 int main(void){
 
-  FILE *arquivo_entrada;
-  FILE *arquivo_saida;
-  arquivo_saida = fopen("utf32Saida.dat", "wb+");
-  arquivo_entrada = fopen("utf8.bin", "rb");
+    FILE *arquivo_entrada;
+    FILE *arquivo_saida;
+    arquivo_saida = fopen("utf32Saida.bin", "wb");
+    arquivo_entrada = fopen("utf8.bin", "rb");
+    unsigned int aux;
+
+    for(aux = fgetc(arquivo_entrada); aux !=EOF; aux = fgetc(arquivo_entrada)){
+        int u8_size = utf8_size(aux);
+
+        unsigned int unicodeVet[u8_size];
+        switch (u8_size){
+            case(1):
+                unicodeVet[0] = 0x00;
+                unicodeVet[1] = 0x00;
+                unicodeVet[2] = 0x00;
+                unicodeVet[3] = 0x00 & (aux>>2);
+            case(2):
+                unicodeVet[0] = 0x00;
+                unicodeVet[1] = 0x00;
+                unicodeVet[2] = 0x00;
+                unicodeVet[3] = 0x00 & (aux>>2);
+            case(3):
+                unicodeVet[0] = 0x00;
+                unicodeVet[1] = 0x00;
+                unicodeVet[2] = 0x00;
+                unicodeVet[3] = 0x00 & (aux>>2);
+            case(4):
+                unicodeVet[0] = 0x00;
+                unicodeVet[1] = 0x00;
+                unicodeVet[2] = 0x00;
+                unicodeVet[3] = 0x00 & (aux>>2);
+            default:
+                printf("Fora do alcance");
+                
+        }
+
+
+    }
+
+    fclose(arquivo_saida);
+    fclose(arquivo_entrada);
+    return 0;
+}
+
+
+/*
   //ler primeiros 4 bytes =  ler 4 char 
 
   //essa funcao ta +- certa, ela ta lendo a cada 4 bytes, precisamos arrumar ela pra ela ler byte por byte e ver se é char 1, 2,3 ou 4 bytes, dai traduzir 
-  int numChar = countChar(arquivo_entrada);
-    unsigned int auxLeitura;   //caracter que le byte por byte 
-    char byteSaida[4] = {0x00, 0x00, 0x00, 0x00};
-
+    unsigned int  auxLeitura = 0;   //caracter que le byte por byte 
+    char bom [4] = {0xFF, 0xFE, 0x00, 0x00};
+    fwrite(bom, sizeof(bom),1, arquivo_saida);
+    
     for(auxLeitura = fgetc(arquivo_entrada); auxLeitura !=EOF; auxLeitura = fgetc(arquivo_entrada)){
-        //realizei a leitura utilizando unsigned int, ja que o caracter de maior tamanho e 4 bytes
-            //8 bists char 
+            char byteSaida[4] = {0x00, 0x00, 0x00, 0x00};
+
         if(auxLeitura <= 0x7F){
             byteSaida[0] = auxLeitura;
+            fwrite(byteSaida,sizeof(char),1,arquivo_saida);
             //16 bits char
-            fwrite(byteSaida, sizeof(char), 4, arquivo_saida);
-            char byteSaida[4] = {0x00, 0x00, 0x00, 0x00};
-        }else if(auxLeitura <= 0x7FF){
+        }else if(auxLeitura >= 0x80 && auxLeitura <= 0x7FF){
             byteSaida[0] = 0xC0 | (auxLeitura >> 6); //110xxxxx || auxLeitura 6 bits pra direita para armazenar os dados do 1 byte
-            byteSaida[1] = 0x80 | (auxLeitura & 0x3F); //10xxxxxx || auxLeitura & 0x3F
-            fwrite(byteSaida, sizeof(char), 4, arquivo_saida);
-            char byteSaida[4] = {0x00, 0x00, 0x00, 0x00};
+            byteSaida[1] = 0x80 | (auxLeitura & 0x3F); //10xxxxxx || auxLeitura & 0x3F            
             //24 bits char
-        }else if(auxLeitura <=0xFFFF){
+            fwrite(byteSaida,sizeof(char),1,arquivo_saida);
+
+        }else if(auxLeitura >=0x800 && auxLeitura <= 0xFFFF){
             byteSaida[0] = 0xE0 | (auxLeitura >> 12); //1110xxxx || 1 byte auxLeitura 3 bits pra direita
             byteSaida[1] = 0x80 | ((auxLeitura >> 6) & 0x3F); //10xxxxxx || auxLeitura 2 bits para direita 
             byteSaida[2] = 0x80 | (auxLeitura & 0X3F); //10xxxxxx || auxLeitura 2 bits pra direita
-            fwrite(byteSaida, sizeof(char), 4, arquivo_saida);
-            char byteSaida[4] = {0x00, 0x00, 0x00, 0x00};
+                        fwrite(byteSaida,sizeof(char),1,arquivo_saida);
+
             //32 bits
-        }else if(auxLeitura <= 0x10FFFF){
+        }else if(auxLeitura >=0x10000 && auxLeitura <= 0x10FFFF){
             byteSaida[0] = 0xF0 | (auxLeitura >> 18); //11110xxx ||  1 byte auxLeitura 3 bits pra direita
             byteSaida[1] = 0x80 | ((auxLeitura >> 12) & 0X3F); //10xxxxxx || auxLeitura 2 bits para direita 
             byteSaida[2] = 0x80 | ((auxLeitura >> 6) & 0X3F); //10xxxxxx || auxLeitura 2 bits pra direita
-            byteSaida[2] = 0x80 | (auxLeitura & 0x3F); //10xxxxxx || auxLeitura 2 bits pra direita
-            fwrite(byteSaida, sizeof(char), 4, arquivo_saida);
-            char byteSaida[4] = {0x00, 0x00, 0x00, 0x00};
+            byteSaida[3] = 0x80 | (auxLeitura & 0x3F); //10xxxxxx || auxLeitura 2 bits pra direita
+            fwrite(byteSaida, sizeof(byteSaida), 1, arquivo_saida);
         }else{
-            printf("Caracter fora do alcance.c\n");
+            printf("Caracter invalido");
         }
-    }
-  fclose(arquivo_saida);
-  fclose(arquivo_entrada);
-  return 0;
-}
-
-/*
 
 */
