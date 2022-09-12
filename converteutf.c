d2c2031c7af8055ef318f09b07e7be8293845926
Leo Lomardo <leoland771@gmail.com>
2022-09-11 21:31:16 -0300

    aa

--- converteutf.c
+++ converteutf.c
@@ -1,1 +1,21 @@
+/* Leo Lomardo - 2020201 - 3WA */
+/* Lucas Lucena -  - 3WA  */
+
 #include "converteutf.h"
+
+
+int converteUtf8Para32(FILE *arquivo_entrada, FILE *arquivo_saida){
+
+}
+
+int converteUtf32Para8(FILE *arquivo_entrada, FILE *arquivo_saida){
+    //ler primeiros 4 bytes =  ler 4 char 
+    unsigned int bom  = 0xFFFE0000;
+    
+    unsigned int check;
+
+    fread(&check, sizeof(int),1, arquivo_entrada);
+    printf("%d", check);
+    
+    
+}
--- testeconv.c
+++ testeconv.c
@@ -1,1 +1,20 @@
 #include "converteutf.h"
+#include <stdio.h>
+
+
+int main(void){
+    FILE *arquivo_entrada;
+
+
+    arquivo_entrada = fopen("utf32.bin", "rb");
+       //ler primeiros 4 bytes =  ler 4 char 
+     int bom  = 0xFFFE0000;
+     int check = 0;
+
+    fread(&check, sizeof(int),1, arquivo_entrada);
+    printf("%d", check);
+    
+
+    fclose(arquivo_entrada);
+    return 0;
+}
